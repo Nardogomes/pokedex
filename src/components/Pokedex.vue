bdc81f8d8ebad0cb28df2270cb52e2c68afc9002
Ednardo Gomes <ednardo@gomes.com>
2021-08-12 11:32:55 -0300

    Criando o componente Divider e configurando seus estilos.

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -2,8 +2,8 @@
 	<div class="pokedex">
 		<TopPanel />
 		<LeftPanel class="pokedex--left" />
-		<!-- <Divider />
-    <RightPanel class="pokedex--right" /> -->
+		<Divider />
+    <!-- <RightPanel class="pokedex--right" /> -->
 	</div>
 </template>
 
@@ -11,15 +11,15 @@
 import TopPanel from "./TopPanel/TopPanel.vue";
 import LeftPanel from "./LeftPanel/LeftPanel.vue";
 // import RightPanel from './RightPanel/RightPanel.vue';
-// import Divider from './Divider/Divider.vue';
+import Divider from './Divider/Divider.vue';
 
 export default {
 	name: "Pokedex",
 	components: {
 		TopPanel,
-		LeftPanel
+		LeftPanel,
 		// RightPanel,
-		// Divider,
+		Divider,
 	}
 };
 </script>
