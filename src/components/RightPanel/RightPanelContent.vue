fc55091b54628170c5ecdaf9ee61cdcc4bfede19
Ednardo Gomes <ednardo@email.com>
2021-08-16 19:34:07 -0300

    iniciando configuração do RightPanel.

--- src/components/RightPanel/RightPanelContent.vue
+++ src/components/RightPanel/RightPanelContent.vue
@@ -1,13 +1,16 @@
 <template>
-  
+	<form class="search">
+		<label for="search" class="search__label">
+			<h3 class="search__label--text">E.g.: Charizard</h3>
+			<input id="search" type="text" class="search__input" />
+		</label>
+
+		<button class="search__button" @clich.prevent>Search</button>
+	</form>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
