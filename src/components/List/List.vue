c3a6783238312515fbf904d47a0ff361931912db
Ednardo Gomes <ednardo@email.com>
2021-08-18 20:12:58 -0300

    Criando a descrição dos pokemons.

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -3,7 +3,7 @@
 		<p v-if="isSearching" class="list-message">Looking for the pokemon</p>
 		<p v-else-if="hasSearchError" class="list-message">We couldn't find this pokemon</p>
 		<ListItem v-else-if="isPokemonSearch" v-bind="pokemonsList[0]" />
-		<PokemonDescription v-else-if="pokemonId" :id="pokemonId"/>
+		<PokemonDescription v-else-if="pokemonId" :id="pokemonId" />
 		<template v-else>
 			<ListItem v-for="pokemon in pokemonsList" :key="pokemon.id" v-bind="pokemon" />
 			<infinite-loading @infinite="infiniteHandler" />
@@ -14,11 +14,13 @@
 <script>
 import { state, getters, actions } from "@/store";
 import ListItem from "./ListItem.vue";
+import PokemonDescription from "@/components/PokemonDescription/PokemonDescription.vue";
 
 export default {
 	name: "List",
 	components: {
-		ListItem
+		ListItem,
+		PokemonDescription
 	},
 	computed: {
 		pokemonsList() {
@@ -32,6 +34,9 @@ export default {
 		},
 		hasSearchError() {
 			return state.searchHasError;
+		},
+		pokemonsId() {
+			return state.pokemonId;
 		}
 	},
 	methods: {
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,13 +1,9 @@
 /* Retona o estado modificado da variáveis */
-import state from './state';
+import { parsePokemonInfo } from "@/utils";
+import state from "./state";
 
 export default {
-  get pokemonsInfo() {
-    return state.list.map(info => ({
-      id: info.id,
-      name: info.name,
-      types: info.types.map(({type}) => type.name),
-      sprite: info.sprites.front_default || 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png',
-    }));
-  },
-}
+	get pokemonsInfo() {
+		return state.list.map(parsePokemonInfo);
+	}
+};
